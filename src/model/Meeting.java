6b94f06e84e42e35aba067e6f4ab7b3e41c2cab9
Salifu Mutaru <salifumutaru@gmail.com>
2014-12-04 23:38:36 +0000

    Meeting class completed

---
+++ src/model/Meeting.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author salifu
+ */
+public class Meeting {
+    
+    private String id;
+    private enum kind{academic, personal};
+    kind thekind;
+    String attend;
+    
+    public Meeting(String id, String k, String attend){
+        this.id = id; 
+        setkind(k); 
+        this.attend = attend;
+    }
+    
+    //kind
+    public void setkind(String k){
+        switch(k){
+            case "academic": thekind = kind.academic;
+                break;
+            case "personal": thekind = kind.personal;
+                break;
+        }
+    }
+    public kind getkind(){
+        return thekind;
+    }
+    
+    public void setattend(String attend){
+        this.attend = attend;
+    }
+    public String getattend(){
+        return attend;
+    }
+    
+}
