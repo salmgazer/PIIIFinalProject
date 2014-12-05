c9fd45094d01154158da008f8f2fe5129c011de1
Salifu Mutaru <salifumutaru@gmail.com>
2014-12-05 00:20:51 +0000

    Member class completed

---
+++ src/model/Goal.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author salifu
+ */
+public class Goal {
+    
+    private String id;
+    private enum kind{group, personal};
+    kind thekind;
+    String goal;
+    String date_entered;
+    String date_to_finish;
+    int activity;
+    
+    public Goal(){
+        
+    }
+    
+    //kind
+    public  void setkind(String k){
+        switch(k){
+            case "group": thekind = kind.group;
+                break;
+            case "personal": thekind = kind.personal;
+                break;
+        }
+    }
+    public kind getkind(){
+        return thekind;
+    }
+    
+    //id
+    public String getid(){
+        return id;
+    }
+    
+    //goal
+    public String getgoal(){
+        return goal;
+    }
+    public void setgoal(String g){
+        goal = g;
+    }
+    
+    //date_enetred
+    public void entered(String d){
+        date_entered = d;
+    }
+    public String getentered(){
+        return date_entered;
+    }
+    
+    //date_to_finish
+    public void tofinish(String f){
+        date_to_finish = f;
+    }
+    public String gettofinish(){
+        return date_to_finish;
+    }
+    
+    //activity
+    public void setactivity(int a){
+        activity = a;
+    }
+    public int getactivity(){
+        return activity;
+    }
+}
